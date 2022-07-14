3745429c8081109c424cf88b3164bcd21490560a
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2022-07-14 18:09:25 +0800

    Change to new website URL

--- src/main/java/me/soopyboo32/soopyv2forge/SoopyV2Forge.java
+++ src/main/java/me/soopyboo32/soopyv2forge/SoopyV2Forge.java
@@ -30,7 +30,7 @@ import java.util.zip.ZipInputStream;
 public class SoopyV2Forge
 {
     public static final String MODID = "soopyv2forge";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     public static SoopyV2Forge INSTANCE;
 
@@ -100,7 +100,7 @@ public class SoopyV2Forge
 
             new Thread(() -> {
                 try {
-                    URL downloadURL = new URL("http://soopymc.my.to/api/soopyv2/downloadlatest.zip");
+                    URL downloadURL = new URL("http://soopy.dev/api/soopyv2/downloadlatest.zip");
 
                     if (!new File("." + File.separator + "config" + File.separator + "ChatTriggers" + File.separator
                             + "modules").exists())
